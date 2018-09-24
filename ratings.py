3b9c13843c2552b827307c317164fb382d78f8e9
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-24 16:23:18 -0700

    Added comments and made sorted_restaurants into a tuple instead of previous method

--- ratings.py
+++ ratings.py
@@ -1,8 +1,12 @@
-"""Restaurant rating lister."""
+
+
+
 def restaurant_rating(filename):
+    """Restaurant rating lister."""
+
     filename = open(filename)
     restaurant_rating = {}
-    restaurant_name = input("Please enter a restaurant name. ").capitalize()
+    restaurant_name = input("Please enter a restaurant name. ").title()
     restaurant_score = int(input("Please enter the restaurant's score. "))
 
     for line in filename:
@@ -10,15 +14,14 @@ def restaurant_rating(filename):
         line = line.split(":")
         restaurant_rating[line[0]] = line[1]
 
+    # add user input to the dictionary
     restaurant_rating[restaurant_name] = restaurant_score
 
-    sorted_restaurant = sorted(restaurant_rating) # sorted list of keys
-    restaurant_rating2 = {}
+    # returns a list of tuples that are the key, value pairs.
+    # automatically sorts based on the first item of each tuple
+    sorted_restaurants = sorted(restaurant_rating.items())  
 
-    for key in sorted_restaurant:
-        restaurant_rating2[key] = restaurant_rating[key]
-
-    for name, rating in restaurant_rating2.items():
+    for name, rating in sorted_restaurants:
         print('{} is rated at {}'.format(name, rating))
 
 
