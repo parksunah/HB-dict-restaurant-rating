34e7e69b26e48daf8f4fb3b42ea09a8973e86806
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-24 16:28:43 -0700

    Validate New Score

--- ratings.py
+++ ratings.py
@@ -8,14 +8,20 @@ def restaurant_rating(filename):
     restaurant_rating = {}
     restaurant_name = input("Please enter a restaurant name. ").title()
     restaurant_score = int(input("Please enter the restaurant's score. "))
+    
+    if 1 <= restaurant_score <= 5:
+        # add user input to the dictionary
+        restaurant_rating[restaurant_name] = restaurant_score
+    
+    else:
+        restaurant_score = int(input("Please enter score between 1 and 5. "))
+        restaurant_rating[restaurant_name] = restaurant_score
 
     for line in filename:
         line = line.rstrip()
         line = line.split(":")
         restaurant_rating[line[0]] = line[1]
-
-    # add user input to the dictionary
-    restaurant_rating[restaurant_name] = restaurant_score
+ 
 
     # returns a list of tuples that are the key, value pairs.
     # automatically sorts based on the first item of each tuple
