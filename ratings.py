4b3352247542a9aa592c7b41c9e3ff96296970fd
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-24 16:12:27 -0700

    Added user input of restaurant and score

--- ratings.py
+++ ratings.py
@@ -2,10 +2,15 @@
 def restaurant_rating(filename):
     filename = open(filename)
     restaurant_rating = {}
+    restaurant_name = input("Please enter a restaurant name. ").capitalize()
+    restaurant_score = int(input("Please enter the restaurant's score. "))
+
     for line in filename:
         line = line.rstrip()
         line = line.split(":")
         restaurant_rating[line[0]] = line[1]
+
+    restaurant_rating[restaurant_name] = restaurant_score
 
     sorted_restaurant = sorted(restaurant_rating) # sorted list of keys
     restaurant_rating2 = {}
@@ -14,7 +19,7 @@ def restaurant_rating(filename):
         restaurant_rating2[key] = restaurant_rating[key]
 
     for name, rating in restaurant_rating2.items():
-        print('{}is rated at {}'.format(name, rating))
+        print('{} is rated at {}'.format(name, rating))
 
 
 restaurant_rating("scores.txt")
